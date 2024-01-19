99ea79968c7f9153149b7342ba5c217a713fb71e
Jumman04 <arsharifuddinjumman04@gmail.com>
2024-01-19 17:21:27 +0600

    Fixed and Optimized.

--- app/src/main/java/com/example/nirbachon/OnBoardActivity.java
+++ app/src/main/java/com/example/nirbachon/OnBoardActivity.java
@@ -1,15 +1,15 @@
 package com.example.nirbachon;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class OnBoardActivity extends AppCompatActivity {
 
-    Button button_english , button_bangla ;
+    Button button_english, button_bangla;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,21 +19,9 @@ public class OnBoardActivity extends AppCompatActivity {
         button_english = findViewById(R.id.button_english);
         button_bangla = findViewById(R.id.button_bangla);
 
-         button_english.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View view) {
+        button_english.setOnClickListener(view -> startActivity(new Intent(OnBoardActivity.this, MainActivity.class)));
 
-                 startActivity(new Intent(OnBoardActivity.this,MainActivity.class));
-             }
-         });
-
-        button_bangla.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                startActivity(new Intent(OnBoardActivity.this,MainActivity.class));
-            }
-        });
+        button_bangla.setOnClickListener(view -> startActivity(new Intent(OnBoardActivity.this, MainActivity.class)));
 
     }
 }
--- app/src/main/java/com/example/nirbachon/SplashActivity.java
+++ app/src/main/java/com/example/nirbachon/SplashActivity.java
@@ -1,28 +1,24 @@
 package com.example.nirbachon;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class SplashActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private final int SPLASH_DISPLAY_LENGTH = 3000;
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashActivity.this,OnBoardActivity.class);
-                SplashActivity.this.startActivity(mainIntent);
-                SplashActivity.this.finish();
-            }
+        int SPLASH_DISPLAY_LENGTH = 3000;
+        new Handler().postDelayed(() -> {
+            /* Create an Intent that will start the Menu-Activity. */
+            Intent mainIntent = new Intent(SplashActivity.this, OnBoardActivity.class);
+            SplashActivity.this.startActivity(mainIntent);
+            SplashActivity.this.finish();
         }, SPLASH_DISPLAY_LENGTH);
 
     }
